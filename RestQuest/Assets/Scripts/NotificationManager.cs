4df3542d8c8fc0368138eb4cb019bc9d26a30506
Kay <kay.dumler@outlook.de>
2024-07-02 22:46:25 +0200

    Partial Fix Message System
    PseudoCode for missing elements not complete

--- RestQuest/Assets/Scripts/NotificationManager.cs
+++ RestQuest/Assets/Scripts/NotificationManager.cs
@@ -8,14 +8,21 @@ public class NotificationManager : MonoBehaviour
 {
     void Start()
     {
+        Snoozed = false;
+        Playtime = 0f;
+        BreakDelay = 30f;         // sollte theoretisch anpassbar sein
+        BreakIntervalSeconds = BreakInterval * 60;
+        AppointmentAheadSeconds = AppointmentAhead * 60;
+        //StartCoroutine(Playtime());
         //Empty Queue 
         // Get Todays-Apointments
         // Add all of them to the queue
+
     }
 
     public TMP_Text MessageTitle;
     public TMP_Text MessageText;
-    public TMP_Text SnoozedMessageTitle;
+    // public TMP_Text SnoozedMessageTitle;
     public TMP_Text SnoozedMessageText;
 
     public GameObject NotificationWindow;
@@ -24,35 +31,137 @@ public class NotificationManager : MonoBehaviour
     private string Appointment = "Appointment";
     private string Break = "Take a break";
 
+    private bool Snoozed;
+    private bool AppointNext;
+
     //private Queue<string> popupQue;
     //private Coroutine queueChecker;
 
     private float Playtime;
-    public float BreakInterval;
+    [Range(5, 60)] public int BreakInterval;
+    [Range(5, 60)] public int AppointmentAhead;
+    private float BreakIntervalSeconds;
+    private float AppointmentAheadSeconds;
+    private float BreakDelay;
+    private float BreakTime;
+    private float AppointmentClose;
 
+    private void ShowBreakNotif()
+    {
+        if (Snoozed == false)
+        {
+            NotificationWindow.SetActive(true);
+            MessageTitle.text = Break;
+            StartCoroutine(BreakTimeCount());
+            // MessageText.text = variable mit der info;
+
+        }
+        else
+        {
+            SnoozeWindow.SetActive(true);
+            // SnoozedMessageTitle.text = Break;
+            StartCoroutine(BreakTimeCount());
+            // SnoozedMessageText.text = variable mit der info;
+        }
+    }
+
+    private void ShowAppointmentNotif()
+    {
+        if (Snoozed == false)
+        {
+            NotificationWindow.SetActive(true);
+            MessageTitle.text = Appointment;
+            StartCoroutine(BreakTimeCount());
+            // MessageText.text = variable mit der info;
+
+        }
+        else
+        {
+            SnoozeWindow.SetActive(true);
+            // SnoozedMessageTitle.text = Appointment;
+            StartCoroutine(BreakTimeCount());
+            // SnoozedMessageText.text = variable mit der info;
+        }
+    }
+
+    public void Snooze()
+    {
+        Snoozed = true;
+        NotificationWindow.SetActive(false);
+        SnoozeWindow.SetActive(false);
+        if (AppointNext == false)
+        {
+            StartCoroutine(DelayMessage());
+        }
+        if (AppointNext == true)
+        {
+            StartCoroutine(DelayAppointment());
+        }
+        StopCoroutine(BreakTimeCount());
+        BreakTime = 0f;
+    }
+
+    public void Acknowledged()
+    {
+        Snoozed = false;
+        NotificationWindow.SetActive(false);
+        SnoozeWindow.SetActive(false);
+        // delete message from queue
+        StopCoroutine(BreakTimeCount());
+        BreakTime = 0f;
+        Playtime = 0f;
+    }
+
+
+    private IEnumerator BreakTimeCount()
+    {
+        while (BreakTime <= 300)
+        {
+            BreakTime += 1;
+            yield return new WaitForSeconds(1);
+        }
+
+        if (BreakTime >= 30)
+        {
+            /* for (kps <= 0)
+             * {
+             * yield return new WaitForSeconds(1);
+             * } */
+            Acknowledged();
+        }
+
+    }
+
+    private IEnumerator DelayMessage()
+    {
+        while (BreakDelay >= 0)
+        {
+            BreakDelay -= 1;
+            yield return new WaitForSeconds(1);
+        }
+        ShowBreakNotif();
+        BreakDelay = 300f;
+        StopCoroutine(DelayMessage());
+    }
+
+    private IEnumerator DelayAppointment()
+    {
+        while (BreakDelay >= 0)
+        {
+            BreakTime -= 1f;
+            yield return new WaitForSeconds(1);
+        }
+        ShowAppointmentNotif();
+        BreakDelay = 300f;
+        StopCoroutine(DelayAppointment());
+    }
     //Playtime hochzählen
 
     /* public void AddToQueue(string text)
     {
         popupQue.Enqueue(text);
         //unfinished
-    }
-
-    private void ShowBreakPopup(string text)
-    {
-        BreakWindow.SetActive(true);
-        BreakMessage.text = text;
-        //unfinished
-    }
-
-    private void ShowAppointmentMessage(string text)
-    {
-        AppointmentWindow.SetActive(true);
-        AppointmentMessage.text = text;
-        //unfinished
     } */
-
-
-
-
 }
+
+
